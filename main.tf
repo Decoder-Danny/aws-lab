57f92f917dff7b2b7963b7123775db7cab7da758
Danny Harris <danny.harris997@gmail.com>
2026-03-08 22:35:44 -0500

    putting subnet back to single and removing multiple AZ

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 module "Enclave" {
   source              = "./Modules/vpc"
   VPC-CIDR            = "10.0.0.0/16"
-  public_subnet_cidr  = ["10.0.1.0/24", 10.0.3.0/24"]
+  public_subnet_cidr  = ["10.0.1.0/24"]
   private_subnet_cidr = "10.0.2.0/24"
 }
 
